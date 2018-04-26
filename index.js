2798ac8ca8d2191ee0ff43a4285a3fe1544284d3
Taivt <taivt@eway.vn>
2018-04-26 19:42:18 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// var message = [];
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.emit("send id", { id: socket.id });
+
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+
+        // message.push(msg);
+
+        io.emit('message', { data: msg, id: socket.id });
+    
+    });
+
+    socket.on('disconnect', () => {
+        console.log('disconnected');
+    })
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
